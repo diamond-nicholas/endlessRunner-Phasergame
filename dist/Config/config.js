d81dd1ee6352ad87055d45b8a5ae615b2ba5bbeb
Diamond Nicholas <diamondnicholas154@gmail.com>
2021-10-02 21:48:53 +0100

    fix linters

--- dist/Config/config.js
+++ dist/Config/config.js
@@ -1,7 +1,13 @@
 import Phaser from 'phaser';
+
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+    },
+  },
 };
--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,12 @@
+// import 'regenerator-runtime/runtime';
 import Phaser from 'phaser';
 import config from './Config/config';
-import GameScene from './Scenes/GameScene';
-import BootScene from './Scenes/BootScene';
-import PreloaderScene from './Scenes/PreloaderScene';
-import TitleScene from './Scenes/TitleScene';
-import OptionsScene from './Scenes/OptionsScene';
-import CreditsScene from './Scenes/CreditsScene';
+import GameScene from './scenes/gameScene';
+import BootScene from './scenes/bootScene';
+import PreloaderScene from './scenes/preloaderScene';
+import TitleScene from './scenes/titleScene';
+import GameOverScene from './scenes/gameOverScene';
+import LeaderboardScene from './scenes/leaderboardScene';
 
 class Game extends Phaser.Game {
   constructor() {
@@ -13,10 +14,11 @@ class Game extends Phaser.Game {
     this.scene.add('Boot', BootScene);
     this.scene.add('Preloader', PreloaderScene);
     this.scene.add('Title', TitleScene);
-    this.scene.add('Options', OptionsScene);
-    this.scene.add('Credits', CreditsScene);
+    this.scene.add('GameOver', GameOverScene);
+    this.scene.add('Leaderboard', LeaderboardScene);
     this.scene.add('Game', GameScene);
-    this.scene.start('Game');
+    this.scene.start('Boot');
   }
 }
+
 window.game = new Game();
